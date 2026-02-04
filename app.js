bfee83f56a137e6c493a0bfc534caac09321adc2
diegosousabrito237-jpg <diegosousabrito237@gmail.com>
2026-02-04 17:29:09 -0300

    app.js

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 import { firebaseConfig } from "./firebase-config.js";
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  query,
-  orderBy
-} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getFirestore, collection, getDocs, query, orderBy } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export async function carregarProdutos(){
-  const col = collection(db, "produtos");
-  const q = query(col, orderBy("criadoEm", "desc"));
+  const q = query(collection(db, "produtos"), orderBy("criadoEm", "desc"));
   const snap = await getDocs(q);
-
   return snap.docs.map(d => d.data());
 }
-// app.js placeholder
